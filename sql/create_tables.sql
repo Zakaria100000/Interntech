52f293a54176b840a61102d4fbd17484881f2968
Zakaria100000 <Zakaria.ALIZOUAOUI.dz@viacesi.fr>
2022-03-29 20:28:47 +0100

    Update concerning Data Base tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -4,24 +4,11 @@
 
 
 #------------------------------------------------------------
-# Table: company
-#------------------------------------------------------------
-
-CREATE TABLE company(
-        id            Int  Auto_increment  NOT NULL ,
-        name          Varchar (50) ,
-        field         Varchar (50) ,
-        nb_internship Int
-	,CONSTRAINT company_PK PRIMARY KEY (id)
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
 # Table: role
 #------------------------------------------------------------
 
 CREATE TABLE role(
-        id   TinyINT NOT NULL ,
+        id   Int  Auto_increment  NOT NULL ,
         name Varchar (50) NOT NULL
 	,CONSTRAINT role_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
@@ -32,7 +19,7 @@ CREATE TABLE role(
 #------------------------------------------------------------
 
 CREATE TABLE promo(
-        id   TinyINT NOT NULL ,
+        id   Int  Auto_increment  NOT NULL ,
         name Varchar (50) NOT NULL
 	,CONSTRAINT promo_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
@@ -43,7 +30,7 @@ CREATE TABLE promo(
 #------------------------------------------------------------
 
 CREATE TABLE center(
-        id   TinyINT NOT NULL ,
+        id   Int  Auto_increment  NOT NULL ,
         name Varchar (50) NOT NULL
 	,CONSTRAINT center_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
@@ -59,11 +46,9 @@ CREATE TABLE user(
         lastname  Varchar (50) NOT NULL ,
         email     Varchar (50) NOT NULL ,
         password  Varchar (50) NOT NULL ,
-        center    Varchar (50) ,
-        promo     Varchar (50) ,
-        id_role   TinyINT NOT NULL ,
-        id_center TinyINT NOT NULL ,
-        id_promo  TinyINT
+        id_role   Int NOT NULL ,
+        id_center Int NOT NULL ,
+        id_promo  Int
 	,CONSTRAINT user_PK PRIMARY KEY (id)
 
 	,CONSTRAINT user_role_FK FOREIGN KEY (id_role) REFERENCES role(id)
@@ -88,7 +73,7 @@ CREATE TABLE location(
 #------------------------------------------------------------
 
 CREATE TABLE internship_type(
-        id   TinyINT NOT NULL ,
+        id   Int  Auto_increment  NOT NULL ,
         name Varchar (50) NOT NULL
 	,CONSTRAINT internship_type_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
@@ -99,9 +84,48 @@ CREATE TABLE internship_type(
 #------------------------------------------------------------
 
 CREATE TABLE internship_category(
-        id   TinyINT NOT NULL ,
+        id   Int  Auto_increment  NOT NULL ,
         name Varchar (50) NOT NULL
 	,CONSTRAINT internship_category_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: wishlist
+#------------------------------------------------------------
+
+CREATE TABLE wishlist(
+        id       Int  Auto_increment  NOT NULL ,
+        id_user  Int NOT NULL
+	,CONSTRAINT wishlist_PK PRIMARY KEY (id)
+
+	,CONSTRAINT wishlist_user_FK FOREIGN KEY (id_user) REFERENCES user(id)
+	,CONSTRAINT wishlist_user_AK UNIQUE (id_user)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: company_field
+#------------------------------------------------------------
+
+CREATE TABLE company_field(
+        id   Int  Auto_increment  NOT NULL ,
+        name Varchar (50) NOT NULL
+	,CONSTRAINT company_field_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: company
+#------------------------------------------------------------
+
+CREATE TABLE company(
+        id               Int  Auto_increment  NOT NULL ,
+        name             Varchar (50) ,
+        id_company_field Int NOT NULL
+	,CONSTRAINT company_PK PRIMARY KEY (id)
+
+	,CONSTRAINT company_company_field_FK FOREIGN KEY (id_company_field) REFERENCES company_field(id)
 )ENGINE=InnoDB;
 
 
@@ -114,18 +138,62 @@ CREATE TABLE Internship(
         name                   Varchar (50) NOT NULL ,
         duration               Int ,
         remuneration           Int ,
-        nb_places              Int ,
+        available_places       Int ,
         skills                 Varchar (50) ,
         created_time           Date ,
         id_company             Int NOT NULL ,
         id_location            TinyINT NOT NULL ,
-        id_internship_category TinyINT ,
-        id_internship_type     TinyINT
+        id_internship_category Int ,
+        id_internship_type     Int
 	,CONSTRAINT Internship_PK PRIMARY KEY (id)
 
 	,CONSTRAINT Internship_company_FK FOREIGN KEY (id_company) REFERENCES company(id)
 	,CONSTRAINT Internship_location0_FK FOREIGN KEY (id_location) REFERENCES location(id)
 	,CONSTRAINT Internship_internship_category1_FK FOREIGN KEY (id_internship_category) REFERENCES internship_category(id)
 	,CONSTRAINT Internship_internship_type2_FK FOREIGN KEY (id_internship_type) REFERENCES internship_type(id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: status
+#------------------------------------------------------------
+
+CREATE TABLE status(
+        id   Int  Auto_increment  NOT NULL ,
+        name Varchar (50) NOT NULL
+	,CONSTRAINT status_PK PRIMARY KEY (id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: application
+#------------------------------------------------------------
+
+CREATE TABLE application(
+        id            Int  Auto_increment  NOT NULL ,
+        cv            Varchar (50) NOT NULL ,
+        lm            Varchar (50) NOT NULL ,
+        id_user       Int NOT NULL ,
+        id_Internship Int NOT NULL ,
+        id_status     Int NOT NULL
+	,CONSTRAINT application_PK PRIMARY KEY (id)
+
+	,CONSTRAINT application_user_FK FOREIGN KEY (id_user) REFERENCES user(id)
+	,CONSTRAINT application_Internship0_FK FOREIGN KEY (id_Internship) REFERENCES Internship(id)
+	,CONSTRAINT application_status1_FK FOREIGN KEY (id_status) REFERENCES status(id)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: internship_in_wishlist
+#------------------------------------------------------------
+
+CREATE TABLE internship_in_wishlist(
+        id          Int NOT NULL ,
+        id_wishlist Int NOT NULL
+	,CONSTRAINT internship_in_wishlist_PK PRIMARY KEY (id,id_wishlist)
+
+	,CONSTRAINT internship_in_wishlist_Internship_FK FOREIGN KEY (id) REFERENCES Internship(id)
+	,CONSTRAINT internship_in_wishlist_wishlist0_FK FOREIGN KEY (id_wishlist) REFERENCES wishlist(id)
 )ENGINE=InnoDB;
 
--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -30,3 +30,78 @@ INSERT INTO `internship_category`(`id`, `name`) VALUES
 (2,'Web Development'),
 (3,'Mobile Development'),
 (4,'Network');
+
+-- City Table 
+
+INSERT INTO `location`(`id`, `city`) VALUES 
+(1,'Adrar'),
+(2,'Chlef'),
+(3,'Laghouat'),
+(4,'Oum El Bouaghi'),
+(5,'Batna'),
+(6,'Béjaïa'),
+(7,'Biskra'),
+(8,'Béchar'),
+(9,'Blida'),
+(10,'Bouira	'),
+(11,'Tamanrasset'),
+(12,'Tébessa'),
+(13,'Tlemcen'),
+(14,'Tiaret'),
+(15,'Tizi Ouzou'),
+(16,'Alger'),
+(17,'Djelfa'),
+(18,'Jijel'),
+(19,'Sétif'),
+(20,'Saïda'),
+(21,'Skikda'),
+(22,'Sidi Bel Abbès	'),
+(23,'Annaba'),
+(24,'Guelma'),
+(25,'Constantine'),
+(26,'Médéa'),
+(27,'Mostaganem'),
+(28,'MSila'),
+(29,'Mascara'),
+(30,'Ouargla'),
+(31,'Oran'),
+(32,'El Bayadh'),
+(33,'Illizi'),
+(34,'Bordj Bou Arreridj'),
+(35,'Boumerdès'),		
+(36,'El Tarf'),		
+(37,'Tindouf'),		
+(38,'Tissemsilt'),		
+(39,'El Oued'),		
+(40,'Khenchela'),		
+(41,'Souk Ahras'),		
+(42,'Tipaza'),		
+(43,'Mila'),		
+(44,'Aïn Defla'),		
+(45,'Naâma'),		
+(46,'Aïn Témouchent'),	
+(47,'Ghardaïa'),	
+(48,'Relizane');		
+
+-- Sectors Table 
+
+(1,'Retail business'),
+(2,'Consulting engineering'),
+(3,'Geomatics'),
+(4,'Embedded computing'),
+(5,'Security'),
+(6,'Financial services'),
+(7,'Telecommunications'),
+(8,'Health'),
+(9,'Transport'),
+(10,'Video game');
+ 
+-- Application Status 
+-- Posted, In-review, Refused, Accepted
+(1,'response to an offer has been made by a Student'),
+(2,'Updates'),
+(3,'internship validation form issued by the company'),
+(4,'validation sheet signed by the pilot'),
+(5,'the internship agreements have been issued to the company'),
+(6,'internship agreements have been returned signed');
+
